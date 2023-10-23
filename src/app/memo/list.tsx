ea4448778ac9dfd1f97b6f69dacd7b050a2b1f40
kawa <29882304+kawasaki75@users.noreply.github.com>
2023-10-23 23:31:02 +0900

    Data gets and Manage 231023

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,23 +1,46 @@
 import { View, StyleSheet } from 'react-native'
 import { router, useNavigation } from 'expo-router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { collection, onSnapshot, query, orderBy } from 'firebase/firestore'
 
 import MemoListItem from '../../components/MemoListItem'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
 import LogOutButton from '../../components/LogOutButton'
+import { db, auth } from '../../config'
+import { type Memo } from '../../../types/memo'
 
 const handlePress = (): void => {
   router.push('/memo/create')
 }
 
-const Index = (): JSX.Element => {
+const List = (): JSX.Element => {
+    const [memos, setMemos] = useState<Memo[]>([])
     const navigation = useNavigation()
     useEffect(() => {
       navigation.setOptions({
         headerRight: () => { return <LogOutButton /> }
       })
-    },[])
+    }, [])
+    useEffect(() => {
+      if (auth.currentUser === null) { return }
+      const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
+      const q = query(ref, orderBy('updatedAt', 'desc'))
+      const unsubscribe = onSnapshot(q, (snapshot) => {
+        const remoteMemos: Memo[] = []
+        snapshot.forEach((doc) => {
+          console.log('memo', doc.data())
+          const { bodyText, updatedAt } = doc.data()
+          remoteMemos.push({
+            id: doc.id,
+            bodyText,
+            updatedAt
+          })
+        })
+        setMemos(remoteMemos)
+      })
+      return unsubscribe
+    }, [])
   return (
     <View style={styles.container}>
       <View>
@@ -39,4 +62,4 @@ const styles = StyleSheet.create({
   }
 })
 
-export default Index
+export default List
